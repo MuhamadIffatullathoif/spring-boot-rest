dad57e0e7d069b9653d1f8178399eb95dd22ccb3
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-26 09:16:57 +0700

    04. Adding REST controller component

---
+++ src/main/java/com/iffat/springboot/rest/controllers/ProductController.java
@@ -0,0 +1,54 @@
+package com.iffat.springboot.rest.controllers;
+
+import com.iffat.springboot.rest.entities.Product;
+import com.iffat.springboot.rest.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<?> list() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("{/id}")
+    public ResponseEntity<?> view(@PathVariable Long id) {
+        Optional<Product> optionalProduct = productService.findById(id);
+        if (optionalProduct.isPresent()) {
+            return ResponseEntity.ok(optionalProduct.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Product product = new Product();
+        product.setId(id);
+        Optional<Product> optionalProduct = productService.delete(product);
+        if(optionalProduct.isPresent()) {
+            return ResponseEntity.ok(optionalProduct.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
