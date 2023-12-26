da3be1a206c8b0df55297c36eec304c3f9eb1ba0
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-26 14:27:15 +0700

    13. Assigning roles and encrypting password with BCrypt

--- src/main/java/com/iffat/springboot/rest/services/UserServiceImpl.java
+++ src/main/java/com/iffat/springboot/rest/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.iffat.springboot.rest.services;
 
+import com.iffat.springboot.rest.entities.Role;
 import com.iffat.springboot.rest.entities.User;
 import com.iffat.springboot.rest.repositories.RoleRepository;
 import com.iffat.springboot.rest.repositories.UserRepository;
@@ -7,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,7 +19,7 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private RoleRepository repository;
+    private RoleRepository roleRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -28,6 +31,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User save(User user) {
+
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
+
+        optionalRoleUser.ifPresent(roles::add);
+
+        if(user.isAdmin()) {
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
