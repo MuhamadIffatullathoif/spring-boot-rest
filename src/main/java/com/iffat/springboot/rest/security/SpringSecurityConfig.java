3c9e5ac68f270919f9f5cc705c519e25fc22724a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-27 08:54:18 +0700

    23. Adding the JwtAuthenticationFilter in Spring Security Configuration

--- src/main/java/com/iffat/springboot/rest/security/SpringSecurityConfig.java
+++ src/main/java/com/iffat/springboot/rest/security/SpringSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.iffat.springboot.rest.security;
 
+import com.iffat.springboot.rest.security.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +16,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -26,6 +38,7 @@ public class SpringSecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
